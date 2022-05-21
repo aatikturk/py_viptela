c6161500b147c116fb0054af5dde3443f8e0a5fc
aatikturk <aatikturk@gmail.com>
2022-05-21 19:02:26 +0300

    added exception handling in response_parser.py

--- py_viptela/response_parser.py
+++ py_viptela/response_parser.py
@@ -1,14 +1,23 @@
 import json
+from json.decoder import JSONDecodeError
 
 class Parser(object):
     @staticmethod
     def parseResponse(response):
-        try:
-            raw_data = json.loads(response.text)
-        except:
-            raw_data = {'error':"could not parse response"}
-        if 'data' in raw_data:
-            parsed_data = raw_data['data']
+        if response.text != '':
+            # check for invalid Json data
+            try:
+                raw_data = json.loads(response.text)
+            except JSONDecodeError:
+                error = {'error':"Invalid Json Data"}
+                return error
+            # check raw_data for key "data".
+            # if the it doesn't exist, return raw_data
+            if 'data' in raw_data:
+                parsed_data = raw_data['data']
+            else:
+                parsed_data = raw_data
+            return parsed_data
         else:
-            parsed_data = raw_data
-        return parsed_data
+            # if response is an empty string, return it
+            return response.text
